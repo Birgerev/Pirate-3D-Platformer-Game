b705f948affe420b0c515822b96d4c9c74be1398
Birger E <bibbevidio@gmail.com>
2023-07-13 13:06:28 +0200

    Clamp camera angle

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -9,6 +9,15 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.eulerAngles += new Vector3(Input.GetAxis("Mouse Y") * sensitivity.y, Input.GetAxis("Mouse X") * sensitivity.x, 0);
+        Vector3 cameraMovement = new Vector3(
+            Input.GetAxis("Mouse Y") * sensitivity.y,
+            Input.GetAxis("Mouse X") * sensitivity.x, 
+            0);
+        Vector3 finalEulerAngles = transform.eulerAngles + cameraMovement;
+        
+        //Clamp X-axis
+        finalEulerAngles.x = Mathf.Clamp(finalEulerAngles.x, -89, 89);
+        
+        transform.eulerAngles = finalEulerAngles;
     }
 }
