c770af8424ea563785638ba0770aaa5443363995
Birger E <bibbevidio@gmail.com>
2023-07-13 17:35:54 +0200

    Working swing

--- Assets/RopeSwing.cs
+++ Assets/RopeSwing.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RopeSwing : MonoBehaviour
 {
-    public float ropeStrength;
+    public float heightChangeResistance;
     public float maxRopeLength;
     [Space]
     public Transform raycasterOrigin;
@@ -13,20 +13,51 @@ public class RopeSwing : MonoBehaviour
     
     private Vector3? _ropeAttachPoint;
     private float _ropeLength;
+    
+    
     private Rigidbody _rigidbody;
+    private GroundChecker _groundChecker;
     
-    
-    
-    // Start is called before the first frame update
-    void Awake()
-    {
-        _rigidbody = GetComponent<Rigidbody>();
-    }
-
     // Update is called once per frame
     void Update()
     {
+        TryAttach();
+        TryDetach();
+
+        if (_ropeAttachPoint != null)
+        {
+            RenderRope();
+            ApplyRopeNormalForce();
+        }
+    }
+
+    public void ApplyRopeNormalForce()
+    {
+        float distanceToRopePoint = Vector3.Distance(transform.position, _ropeAttachPoint.Value);
+        if (distanceToRopePoint < _ropeLength)
+            return;
         
+        Vector3 directionToRopePoint = (_ropeAttachPoint.Value - transform.position).normalized;
+        //Get magnitude of force pointing straight away from rope attach point in player velocity
+        float playerOpposingForceStrength = Vector3.Dot(_rigidbody.velocity, -directionToRopePoint);
+        
+        //Return if we are moving towards attach point or are still
+        if (playerOpposingForceStrength <= 0)
+            return;
+
+        //Add rope normal force
+        _rigidbody.velocity += directionToRopePoint * playerOpposingForceStrength;
+    }
+
+    public void RenderRope()
+    {
+        //Render Line
+        lineRenderer.SetPosition(0, ropeVisualOrigin.position);
+        lineRenderer.SetPosition(1, _ropeAttachPoint.Value);
+    }
+    
+    public void TryAttach()
+    {
         if (Input.GetMouseButton(1) && _ropeAttachPoint == null)
         {
             Ray ray = new Ray(raycasterOrigin.position, raycasterOrigin.forward);
@@ -34,43 +65,17 @@ public class RopeSwing : MonoBehaviour
             if(Physics.Raycast(ray, out rayHit, maxRopeLength))
                 AttachRope(rayHit.point);
         }
-
-
-        if(Input.GetMouseButtonUp(1))
-            Detach();
-
-        if (_ropeAttachPoint != null)
-        {
-            //Render Line
-            lineRenderer.SetPosition(0, ropeVisualOrigin.position);
-            lineRenderer.SetPosition(1, _ropeAttachPoint.Value);
-            
-            
-            float distanceToRopePoint = Vector3.Distance(transform.position, _ropeAttachPoint.Value);
-            if (distanceToRopePoint > _ropeLength)
-            {
-                Vector3 directionToRopePoint = (_ropeAttachPoint.Value - transform.position).normalized;
-                //Get magnitude of force pointing straight away from rope attach point in player velocity
-                float playerOpposingForceStrength = Vector3.Dot(_rigidbody.velocity, -directionToRopePoint);
-                
-                //Add rope normal force
-                _rigidbody.velocity += directionToRopePoint * playerOpposingForceStrength;
-            }
-            
-            /*
-            float distanceToRopePoint = Vector3.Distance(transform.position, _ropeAttachPoint.Value);
-            if (distanceToRopePoint > _ropeLength)
-            {
-                //float ropeOverstretchLength = distanceToRopePoint - _ropeLength;
-                Vector3 directionToRope = (_ropeAttachPoint.Value - transform.position).normalized;
-                //float ropeCentripetalForceStrength = ropeOverstretchLength * ropeStrength ;
-                
-                _rigidbody.AddForce(
-                    directionToRope * ropeStrength * Time.fixedTime * Time.timeScale);
-            }*/
-        }
     }
 
+    public void TryDetach()
+    {
+        if(Input.GetMouseButtonUp(1))
+            Detach();
+        
+        if(_groundChecker.IsGrounded())
+            Detach();
+    }
+    
     public void AttachRope(Vector3 attachPoint)
     {
         _ropeAttachPoint = attachPoint;
@@ -82,4 +87,12 @@ public class RopeSwing : MonoBehaviour
         _ropeAttachPoint = null;
         _ropeLength = 0;
     }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _groundChecker = GetComponent<GroundChecker>();
+    }
+
 }
